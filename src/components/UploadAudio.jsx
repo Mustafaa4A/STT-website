36ccc637c9aa69a23af6ce640833623e31d15bdb
Mustafaa4a <musdhafa42@gmail.com>
2023-05-28 06:50:34 +0300

    Adding file upload and drop

--- src/components/UploadAudio.jsx
+++ src/components/UploadAudio.jsx
@@ -1,53 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // reactstrap components
 import {
   Card,
   CardBody
 } from "reactstrap";
-import Dropzone from 'react-dropzone';
+import { useDropzone } from 'react-dropzone';
 
 const UploadAudio = () => {
-  const getUploadParams = () => {
-    return { url: 'https://httpbin.org/post' }
+   const { getRootProps, getInputProps } = useDropzone({
+     accept: {
+      'audio/*':['.wav', '.mp3', '.mp4', '.caf', '.afif', '.avi']
+    },
+    onDrop: (acceptedFiles) => {
+      onDropFile(acceptedFiles[0])
+    },
+  });
+  const [ file, setFile ] = useState(null);
+  
+
+  const onDropFile = (file) => {
+    setFile(file)
+    console.log(file)
   }
 
-  const handleChangeStatus = ({ meta }, status) => {
-    console.log(status, meta)
-  }
-
-  const handleSubmit = (files, allFiles) => {
-    console.log(files.map(f => f.meta))
-    allFiles.forEach(f => f.remove())
-  }
   return (
-    <Dropzone
-      getUploadParams={getUploadParams}
-      onChangeStatus={handleChangeStatus}
-      onSubmit={handleSubmit}
-      styles={{ dropzone: { minHeight: 200, maxHeight: 250 } }}
-    >
-      {({ getRootProps, getInputProps }) => (
       <Card className="shadow  mt-5 " {...getRootProps()} >
-        <input {...getInputProps()} />
+        <input {...getInputProps()} accept='audio/wav' />
       <CardBody style={{cursor:"pointer"}}>
         <div className="d-flex flex-column px-3 justify-content-center align-items-center" style={{ minHeight: "230px" }}>
           <div className='pb-3'>
             <div className="icon icon-shape bg-gradient-primary rounded-circle text-white">
-              <i className="fa fa-cloud-upload" />
+                {
+                  !file?
+                  <i className="fa fa-cloud-upload" />
+                  :
+                  <i className="fa fa-edit" />
+                }
             </div>
           </div>
           <div className="text-center">
-            <h5 className="title">
-              Upload Audio File
-              </h5>
-              <p>Supported Formats: WAV, MP3, M4A, CAF, AIFF, AVI, RMVB, FLV, MP4, MOV, WMV</p>
+            {
+                  !file ? (
+                    <>
+                      <h5 className="title">Upload/Drag  Audio File</h5>
+                      <p>Supported Formats: WAV, MP3,  CAF, AIFF, AVI</p>
+                    </>
+                  ) : (
+                      <p>{file?.path}</p>
+              )      
+            }
           </div>
         </div>
       </CardBody>
     </Card>
-        )}
-    </Dropzone>
   )
 }
 
